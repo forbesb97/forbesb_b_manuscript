fac5eeeac5cb03c481589f2b5395adf10db88ceb
Brieanne <forbesb@sonoma.com>
2021-04-26 11:38:55 -0700

    add all variables for all methods (1-2m, 2-3m, 3-4m, 4-5m, 5-6m, 6-7m, 7-8m)

--- ladder-fuel-metrics_density_tls_als_uav_zeb(3).R
+++ ladder-fuel-metrics_density_tls_als_uav_zeb(3).R
@@ -1,6 +1,6 @@
 # ==============================================================================
 #
-# TLS, UAV, ALS, ZEB ladder fuel calculation using density 
+# TLS, uas, ALS, ZEB ladder fuel calculation using density 
 #
 # ==============================================================================
 #
@@ -15,8 +15,8 @@
 #
 # Description:
 #
-# Computes ladder fuel metrics as defined by TLS from TLS, HMLS (zeb), ALS, UAS
-# 
+# Computes ladder fuel metrics  TLS, HMLS (zeb), ALS, UAS
+# 1-2m, 2-3m, 3-4m, 4-5m, 5-6m, 6-7m, 7-8m
 #
 # ==============================================================================
 #
@@ -25,7 +25,6 @@
 # tls_las_folder = Folder location for TLS .las files
 # tls_las_files = list.files function to get tls files to be processed. Can be modified
 #   with a pattern to restrict search
-# resolutions = Vector of resolutions (in meters) to use for chm and canopy cover
 # out_file = output .csv file name
 #
 # ==============================================================================
@@ -53,18 +52,18 @@ tls_las_files <- list.files(tls_las_folder, pattern = '.las', full.names = TRUE)
 als_las_folder <- 'D:/c1 - Pepperwood/c1_ALS_normplot'
 als_las_files <- list.files(als_las_folder, pattern = '.las', full.names = TRUE)
 
-uav_las_folder <- 'D:/c1 - Pepperwood/c1_uas'
-uav_las_files <- list.files(uav_las_folder, pattern = '.las', full.names = TRUE)
+uas_las_folder <- 'D:/c1 - Pepperwood/c1_uas'
+uas_las_files <- list.files(uas_las_folder, pattern = '.las', full.names = TRUE)
 
 zeb_las_folder <- 'D:/c1 - Pepperwood/c1_zeb_cut2plot'
 zeb_las_files <- list.files(zeb_las_folder, pattern = '.las', full.names = TRUE)
 
-banner_data <- read_csv('D:/Analyses/Ladder fuels-airtable.csv')
+banner_data <- read_csv('D:/Analyses/ladder_fuel_jmp/Ladder fuels_airtable_210426.csv')
 
 
 # out_file <- 'data/voxel_ladder-fuels.csv'
-out_file <- 'D:/Analyses/ladder_fuel_full_process_take4/not_filtered/c1_ladder-fuels_metrics_tls_zeb_als_uas_210326.csv'
-out_file_filtered <- 'D:/Analyses/ladder_fuel_full_process_take4/filtered_zero_and_above/c1_ladder-fuels_metrics_tls_zeb_als_uas_filtered_210326.csv'
+out_file <- 'D:/Analyses/ladder_fuel_jmp/c1_ladder-fuels_metrics_tls_zeb_als_uas_notfiltered_210426.csv'
+out_file_filtered <- 'D:/Analyses/ladder_fuel_jmp/c1_ladder-fuels_metrics_tls_zeb_als_uas_filtered_210426.csv'
 
 # ============ Compute density based ladder fuels metrics for TLS data ===========
 
@@ -73,8 +72,11 @@ tls_combine <- tibble(
   campaign = numeric(),
   plot = numeric(),
   tls_ladder_fuel_1to2 = numeric(),
-  tls_ladder_fuel_1to3 = numeric(),
-  tls_ladder_fuel_1to4 = numeric(),
+  tls_ladder_fuel_2to3 = numeric(),
+  tls_ladder_fuel_3to4 = numeric(),
+  tls_ladder_fuel_4to5 = numeric(),
+  tls_ladder_fuel_5to6 = numeric(),
+  tls_ladder_fuel_6to7 = numeric(),
   tls_ladder_fuel_7to8 = numeric()
 )
 
@@ -82,8 +84,11 @@ tls_combine_filter <- tibble(
   campaign = numeric(),
   plot = numeric(),
   tls_ladder_fuel_1to2 = numeric(),
-  tls_ladder_fuel_1to3 = numeric(),
-  tls_ladder_fuel_1to4 = numeric(),
+  tls_ladder_fuel_2to3 = numeric(),
+  tls_ladder_fuel_3to4 = numeric(),
+  tls_ladder_fuel_4to5 = numeric(),
+  tls_ladder_fuel_5to6 = numeric(),
+  tls_ladder_fuel_6to7 = numeric(),
   tls_ladder_fuel_7to8 = numeric()
 )
 
@@ -103,8 +108,11 @@ for (tls_file in tls_las_files) {
     rename(Z='value') %>%
     summarize(
       tls_ladder_fuel_1to2 = (sum(tls_las$Z > 1 & tls_las$Z <= 2) / sum(tls_las$Z <= 2)),
-      tls_ladder_fuel_1to3 = (sum(tls_las$Z > 1 & tls_las$Z <= 3) / sum(tls_las$Z <= 3)),
-      tls_ladder_fuel_1to4 = (sum(tls_las$Z > 1 & tls_las$Z <= 4) / sum(tls_las$Z <= 4)),
+      tls_ladder_fuel_2to3 = (sum(tls_las$Z > 2 & tls_las$Z <= 3) / sum(tls_las$Z <= 3)),
+      tls_ladder_fuel_3to4 = (sum(tls_las$Z > 3 & tls_las$Z <= 4) / sum(tls_las$Z <= 4)),
+      tls_ladder_fuel_4to5 = (sum(tls_las$Z > 4 & tls_las$Z <= 5) / sum(tls_las$Z <= 5)),
+      tls_ladder_fuel_5to6 = (sum(tls_las$Z > 5 & tls_las$Z <= 6) / sum(tls_las$Z <= 6)),
+      tls_ladder_fuel_6to7 = (sum(tls_las$Z > 6 & tls_las$Z <= 7) / sum(tls_las$Z <= 7)),
       tls_ladder_fuel_7to8 = (sum(tls_las$Z > 7 & tls_las$Z <= 8) / sum(tls_las$Z <= 8))
     ) %>%
     add_column(campaign, plot, .before = 1)
@@ -115,10 +123,13 @@ for (tls_file in tls_las_files) {
   tls_metric_filter <- as_tibble(tls_las$Z) %>% 
     rename(Z='value') %>%
     summarize(
-      tls_ladder_fuel_1to2 = (sum(Z > 1 & Z <= 2) / sum(Z >= 0 & Z <= 2)),
-      tls_ladder_fuel_1to3 = (sum(Z > 1 & Z <= 3) / sum(Z >= 0 & Z <= 3)),
-      tls_ladder_fuel_1to4 = (sum(Z > 1 & Z <= 4) / sum(Z >= 0 & Z <= 4)),
-      tls_ladder_fuel_7to8 = (sum(Z > 7 & Z <= 8) / sum(Z >= 0 & Z <= 8))
+      tls_ladder_fuel_1to2 = (sum(tls_las$Z > 1 & tls_las$Z <= 2) / sum(tls_las$Z >= 0 & tls_las$Z <= 2)),
+      tls_ladder_fuel_2to3 = (sum(tls_las$Z > 2 & tls_las$Z <= 3) / sum(tls_las$Z >= 0 & tls_las$Z <= 3)),
+      tls_ladder_fuel_3to4 = (sum(tls_las$Z > 3 & tls_las$Z <= 4) / sum(tls_las$Z >= 0 & tls_las$Z <= 4)),
+      tls_ladder_fuel_4to5 = (sum(tls_las$Z > 4 & tls_las$Z <= 5) / sum(tls_las$Z >= 0 & tls_las$Z <= 5)),
+      tls_ladder_fuel_5to6 = (sum(tls_las$Z > 5 & tls_las$Z <= 6) / sum(tls_las$Z >= 0 & tls_las$Z <= 6)),
+      tls_ladder_fuel_6to7 = (sum(tls_las$Z > 6 & tls_las$Z <= 7) / sum(tls_las$Z >= 0 & tls_las$Z <= 7)),
+      tls_ladder_fuel_7to8 = (sum(tls_las$Z > 7 & tls_las$Z <= 8) / sum(tls_las$Z >= 0 & tls_las$Z <= 8))
     ) %>%
     add_column(campaign, plot, .before = 1)
   
@@ -136,8 +147,11 @@ als_combine <- tibble(
   campaign = numeric(),
   plot = numeric(),
   als_ladder_fuel_1to2 = numeric(),
-  als_ladder_fuel_1to3 = numeric(),
-  als_ladder_fuel_1to4 = numeric(),
+  als_ladder_fuel_2to3 = numeric(),
+  als_ladder_fuel_3to4 = numeric(),
+  als_ladder_fuel_4to5 = numeric(),
+  als_ladder_fuel_5to6 = numeric(),
+  als_ladder_fuel_6to7 = numeric(),
   als_ladder_fuel_7to8 = numeric()
 )
 
@@ -145,8 +159,11 @@ als_combine_filter <- tibble(
   campaign = numeric(),
   plot = numeric(),
   als_ladder_fuel_1to2 = numeric(),
-  als_ladder_fuel_1to3 = numeric(),
-  als_ladder_fuel_1to4 = numeric(),
+  als_ladder_fuel_2to3 = numeric(),
+  als_ladder_fuel_3to4 = numeric(),
+  als_ladder_fuel_4to5 = numeric(),
+  als_ladder_fuel_5to6 = numeric(),
+  als_ladder_fuel_6to7 = numeric(),
   als_ladder_fuel_7to8 = numeric()
 )
 
@@ -165,10 +182,13 @@ for (als_file in als_las_files) {
   als_metric <- as_tibble(als_las$Z) %>% 
     rename(Z='value') %>%
     summarize(
-      als_ladder_fuel_1to2 = sum(Z > 1 & Z <= 2) / sum(Z <= 2),
-      als_ladder_fuel_1to3 = sum(Z > 1 & Z <= 3) / sum(Z <= 3),
-      als_ladder_fuel_1to4 = sum(Z > 1 & Z <= 4) / sum(Z <= 4),
-      als_ladder_fuel_7to8 = sum(Z > 7 & Z <= 8) / sum(Z <= 8),
+      als_ladder_fuel_1to2 = (sum(als_las$Z > 1 & als_las$Z <= 2) / sum(als_las$Z <= 2)),
+      als_ladder_fuel_2to3 = (sum(als_las$Z > 2 & als_las$Z <= 3) / sum(als_las$Z <= 3)),
+      als_ladder_fuel_3to4 = (sum(als_las$Z > 3 & als_las$Z <= 4) / sum(als_las$Z <= 4)),
+      als_ladder_fuel_4to5 = (sum(als_las$Z > 4 & als_las$Z <= 5) / sum(als_las$Z <= 5)),
+      als_ladder_fuel_5to6 = (sum(als_las$Z > 5 & als_las$Z <= 6) / sum(als_las$Z <= 6)),
+      als_ladder_fuel_6to7 = (sum(als_las$Z > 6 & als_las$Z <= 7) / sum(als_las$Z <= 7)),
+      als_ladder_fuel_7to8 = (sum(als_las$Z > 7 & als_las$Z <= 8) / sum(als_las$Z <= 8))
     ) %>%
     add_column(campaign, plot, .before = 1)
   
@@ -178,10 +198,13 @@ for (als_file in als_las_files) {
   als_metric_filter <- as_tibble(als_las$Z) %>% 
     rename(Z='value') %>%
     summarize(
-      als_ladder_fuel_1to2 = sum(Z > 1 & Z <= 2) / sum(Z >= 0 & Z <= 2),
-      als_ladder_fuel_1to3 = sum(Z > 1 & Z <= 3) / sum(Z >= 0 & Z <= 3),
-      als_ladder_fuel_1to4 = sum(Z > 1 & Z <= 4) / sum(Z >= 0 & Z <= 4),
-      als_ladder_fuel_7to8 = sum(Z > 7 & Z <= 8) / sum(Z >= 0 & Z <= 8)
+      als_ladder_fuel_1to2 = (sum(als_las$Z > 1 & als_las$Z <= 2) / sum(als_las$Z >= 0 & als_las$Z <= 2)),
+      als_ladder_fuel_2to3 = (sum(als_las$Z > 2 & als_las$Z <= 3) / sum(als_las$Z >= 0 & als_las$Z <= 3)),
+      als_ladder_fuel_3to4 = (sum(als_las$Z > 3 & als_las$Z <= 4) / sum(als_las$Z >= 0 & als_las$Z <= 4)),
+      als_ladder_fuel_4to5 = (sum(als_las$Z > 4 & als_las$Z <= 5) / sum(als_las$Z >= 0 & als_las$Z <= 5)),
+      als_ladder_fuel_5to6 = (sum(als_las$Z > 5 & als_las$Z <= 6) / sum(als_las$Z >= 0 & als_las$Z <= 6)),
+      als_ladder_fuel_6to7 = (sum(als_las$Z > 6 & als_las$Z <= 7) / sum(als_las$Z >= 0 & als_las$Z <= 7)),
+      als_ladder_fuel_7to8 = (sum(als_las$Z > 7 & als_las$Z <= 8) / sum(als_las$Z >= 0 & als_las$Z <= 8))
     ) %>%
     add_column(campaign, plot, .before = 1)
   
@@ -189,64 +212,76 @@ for (als_file in als_las_files) {
     add_row(als_metric_filter)
   
 }
-# ============ Compute density based ladder fuels metrics for UAV data ===========
+# ============ Compute density based ladder fuels metrics for uas data ===========
 
 
-uav_combine <- tibble(
+uas_combine <- tibble(
   campaign = numeric(),
   plot = numeric(),
-  uav_ladder_fuel_1to2 = numeric(),
-  uav_ladder_fuel_1to3 = numeric(),
-  uav_ladder_fuel_1to4 = numeric(),
-  uav_ladder_fuel_7to8 = numeric()
+  uas_ladder_fuel_1to2 = numeric(),
+  uas_ladder_fuel_2to3 = numeric(),
+  uas_ladder_fuel_3to4 = numeric(),
+  uas_ladder_fuel_4to5 = numeric(),
+  uas_ladder_fuel_5to6 = numeric(),
+  uas_ladder_fuel_6to7 = numeric(),
+  uas_ladder_fuel_7to8 = numeric()
 )
 
-uav_combine_filter <- tibble(
+uas_combine_filter <- tibble(
   campaign = numeric(),
   plot = numeric(),
-  uav_ladder_fuel_1to2 = numeric(),
-  uav_ladder_fuel_1to3 = numeric(),
-  uav_ladder_fuel_1to4 = numeric(),
-  uav_ladder_fuel_7to8 = numeric()
+  uas_ladder_fuel_1to2 = numeric(),
+  uas_ladder_fuel_2to3 = numeric(),
+  uas_ladder_fuel_3to4 = numeric(),
+  uas_ladder_fuel_4to5 = numeric(),
+  uas_ladder_fuel_5to6 = numeric(),
+  uas_ladder_fuel_6to7 = numeric(),
+  uas_ladder_fuel_7to8 = numeric()
 )
 
-for (uav_file in uav_las_files) {
+for (uas_file in uas_las_files) {
   
-  campaign <- str_extract(uav_file, '(?<=c)[:digit:]') %>%
+  campaign <- str_extract(uas_file, '(?<=c)[:digit:]') %>%
     as.numeric()
-  plot <- str_extract(uav_file, '(?<=p)[:digit:]+') %>%
+  plot <- str_extract(uas_file, '(?<=p)[:digit:]+') %>%
     as.numeric()
   
-  message('processing uav campaign ', campaign, ' plot ', plot)
+  message('processing uas campaign ', campaign, ' plot ', plot)
   
-  uav_las <- uav_file %>%
+  uas_las <- uas_file %>%
     readLAS(select = '')
   
-  uav_metric <- as_tibble(uav_las$Z) %>% 
+  uas_metric <- as_tibble(uas_las$Z) %>% 
     rename(Z='value') %>%
     summarize(
-      uav_ladder_fuel_1to2 = sum(uav_las$Z > 1 & uav_las$Z <= 2) / sum(uav_las$Z <= 2),
-      uav_ladder_fuel_1to3 = sum(uav_las$Z > 1 & uav_las$Z <= 3) / sum(uav_las$Z <= 3),
-      uav_ladder_fuel_1to4 = sum(uav_las$Z > 1 & uav_las$Z <= 4) / sum(uav_las$Z <= 4),
-      uav_ladder_fuel_7to8 = sum(uav_las$Z > 7 & uav_las$Z <= 8) / sum(uav_las$Z <= 8)
+      uas_ladder_fuel_1to2 = (sum(uas_las$Z > 1 & uas_las$Z <= 2) / sum(uas_las$Z <= 2)),
+      uas_ladder_fuel_2to3 = (sum(uas_las$Z > 2 & uas_las$Z <= 3) / sum(uas_las$Z <= 3)),
+      uas_ladder_fuel_3to4 = (sum(uas_las$Z > 3 & uas_las$Z <= 4) / sum(uas_las$Z <= 4)),
+      uas_ladder_fuel_4to5 = (sum(uas_las$Z > 4 & uas_las$Z <= 5) / sum(uas_las$Z <= 5)),
+      uas_ladder_fuel_5to6 = (sum(uas_las$Z > 5 & uas_las$Z <= 6) / sum(uas_las$Z <= 6)),
+      uas_ladder_fuel_6to7 = (sum(uas_las$Z > 6 & uas_las$Z <= 7) / sum(uas_las$Z <= 7)),
+      uas_ladder_fuel_7to8 = (sum(uas_las$Z > 7 & uas_las$Z <= 8) / sum(uas_las$Z <= 8))
     ) %>%
     add_column(campaign, plot, .before = 1)
   
-  uav_combine <- uav_combine %>%
-    add_row(uav_metric)
+  uas_combine <- uas_combine %>%
+    add_row(uas_metric)
   
-  uav_metric_filter <- as_tibble(uav_las$Z) %>% 
+  uas_metric_filter <- as_tibble(uas_las$Z) %>% 
     rename(Z='value') %>%
     summarize(
-      uav_ladder_fuel_1to2 = sum(uav_las$Z > 1 & uav_las$Z <= 2) / sum(uav_las$Z >= 0 & uav_las$Z <= 2),
-      uav_ladder_fuel_1to3 = sum(uav_las$Z > 1 & uav_las$Z <= 3) / sum(uav_las$Z >= 0 & uav_las$Z <= 3),
-      uav_ladder_fuel_1to4 = sum(uav_las$Z > 1 & uav_las$Z <= 4) / sum(uav_las$Z >= 0 & uav_las$Z <= 4),
-      uav_ladder_fuel_7to8 = sum(uav_las$Z > 7 & uav_las$Z <= 8) / sum(uav_las$Z >= 0 & uav_las$Z <= 8)
+      uas_ladder_fuel_1to2 = (sum(uas_las$Z > 1 & uas_las$Z <= 2) / sum(uas_las$Z >= 0 & uas_las$Z <= 2)),
+      uas_ladder_fuel_2to3 = (sum(uas_las$Z > 2 & uas_las$Z <= 3) / sum(uas_las$Z >= 0 & uas_las$Z <= 3)),
+      uas_ladder_fuel_3to4 = (sum(uas_las$Z > 3 & uas_las$Z <= 4) / sum(uas_las$Z >= 0 & uas_las$Z <= 4)),
+      uas_ladder_fuel_4to5 = (sum(uas_las$Z > 4 & uas_las$Z <= 5) / sum(uas_las$Z >= 0 & uas_las$Z <= 5)),
+      uas_ladder_fuel_5to6 = (sum(uas_las$Z > 5 & uas_las$Z <= 6) / sum(uas_las$Z >= 0 & uas_las$Z <= 6)),
+      uas_ladder_fuel_6to7 = (sum(uas_las$Z > 6 & uas_las$Z <= 7) / sum(uas_las$Z >= 0 & uas_las$Z <= 7)),
+      uas_ladder_fuel_7to8 = (sum(uas_las$Z > 7 & uas_las$Z <= 8) / sum(uas_las$Z >= 0 & uas_las$Z <= 8))
     ) %>%
     add_column(campaign, plot, .before = 1)
   
-  uav_combine_filter <- uav_combine_filter %>%
-    add_row(uav_metric_filter)
+  uas_combine_filter <- uas_combine_filter %>%
+    add_row(uas_metric_filter)
   
 }
 
@@ -257,8 +292,11 @@ zeb_combine <- tibble(
   campaign = numeric(),
   plot = numeric(),
   zeb_ladder_fuel_1to2 = numeric(),
-  zeb_ladder_fuel_1to3 = numeric(),
-  zeb_ladder_fuel_1to4 = numeric(),
+  zeb_ladder_fuel_2to3 = numeric(),
+  zeb_ladder_fuel_3to4 = numeric(),
+  zeb_ladder_fuel_4to5 = numeric(),
+  zeb_ladder_fuel_5to6 = numeric(),
+  zeb_ladder_fuel_6to7 = numeric(),
   zeb_ladder_fuel_7to8 = numeric()
 )
 
@@ -266,8 +304,11 @@ zeb_combine_filter <- tibble(
   campaign = numeric(),
   plot = numeric(),
   zeb_ladder_fuel_1to2 = numeric(),
-  zeb_ladder_fuel_1to3 = numeric(),
-  zeb_ladder_fuel_1to4 = numeric(),
+  zeb_ladder_fuel_2to3 = numeric(),
+  zeb_ladder_fuel_3to4 = numeric(),
+  zeb_ladder_fuel_4to5 = numeric(),
+  zeb_ladder_fuel_5to6 = numeric(),
+  zeb_ladder_fuel_6to7 = numeric(),
   zeb_ladder_fuel_7to8 = numeric()
 )
 
@@ -286,10 +327,13 @@ for (zeb_file in zeb_las_files) {
   zeb_metric <- as_tibble(zeb_las$Z) %>% 
     rename(Z='value') %>%
     summarize(
-      zeb_ladder_fuel_1to2 = sum(Z > 1 & Z <= 2) / sum(Z <= 2),
-      zeb_ladder_fuel_1to3 = sum(Z > 1 & Z <= 3) / sum(Z <= 3),
-      zeb_ladder_fuel_1to4 = sum(Z > 1 & Z <= 4) / sum(Z <= 4),
-      zeb_ladder_fuel_7to8 = sum(Z > 7 & Z <= 8) / sum(Z <= 8)
+      zeb_ladder_fuel_1to2 = (sum(zeb_las$Z > 1 & zeb_las$Z <= 2) / sum(zeb_las$Z <= 2)),
+      zeb_ladder_fuel_2to3 = (sum(zeb_las$Z > 2 & zeb_las$Z <= 3) / sum(zeb_las$Z <= 3)),
+      zeb_ladder_fuel_3to4 = (sum(zeb_las$Z > 3 & zeb_las$Z <= 4) / sum(zeb_las$Z <= 4)),
+      zeb_ladder_fuel_4to5 = (sum(zeb_las$Z > 4 & zeb_las$Z <= 5) / sum(zeb_las$Z <= 5)),
+      zeb_ladder_fuel_5to6 = (sum(zeb_las$Z > 5 & zeb_las$Z <= 6) / sum(zeb_las$Z <= 6)),
+      zeb_ladder_fuel_6to7 = (sum(zeb_las$Z > 6 & zeb_las$Z <= 7) / sum(zeb_las$Z <= 7)),
+      zeb_ladder_fuel_7to8 = (sum(zeb_las$Z > 7 & zeb_las$Z <= 8) / sum(zeb_las$Z <= 8))
     ) %>%
     add_column(campaign, plot, .before = 1)
   
@@ -299,10 +343,13 @@ for (zeb_file in zeb_las_files) {
   zeb_metric_filter <- as_tibble(zeb_las$Z) %>% 
     rename(Z='value') %>%
     summarize(
-      zeb_ladder_fuel_1to2 = sum(Z > 1 & Z <= 2) / sum(Z >= 0 & Z <= 2),
-      zeb_ladder_fuel_1to3 = sum(Z > 1 & Z <= 3) / sum(Z >= 0 & Z <= 3),
-      zeb_ladder_fuel_1to4 = sum(Z > 1 & Z <= 4) / sum(Z >= 0 & Z <= 4),
-      zeb_ladder_fuel_7to8 = sum(Z > 7 & Z <= 8) / sum(Z >= 0 & Z <= 8)
+      zeb_ladder_fuel_1to2 = (sum(zeb_las$Z > 1 & zeb_las$Z <= 2) / sum(zeb_las$Z >= 0 & zeb_las$Z <= 2)),
+      zeb_ladder_fuel_2to3 = (sum(zeb_las$Z > 2 & zeb_las$Z <= 3) / sum(zeb_las$Z >= 0 & zeb_las$Z <= 3)),
+      zeb_ladder_fuel_3to4 = (sum(zeb_las$Z > 3 & zeb_las$Z <= 4) / sum(zeb_las$Z >= 0 & zeb_las$Z <= 4)),
+      zeb_ladder_fuel_4to5 = (sum(zeb_las$Z > 4 & zeb_las$Z <= 5) / sum(zeb_las$Z >= 0 & zeb_las$Z <= 5)),
+      zeb_ladder_fuel_5to6 = (sum(zeb_las$Z > 5 & zeb_las$Z <= 6) / sum(zeb_las$Z >= 0 & zeb_las$Z <= 6)),
+      zeb_ladder_fuel_6to7 = (sum(zeb_las$Z > 6 & zeb_las$Z <= 7) / sum(zeb_las$Z >= 0 & zeb_las$Z <= 7)),
+      zeb_ladder_fuel_7to8 = (sum(zeb_las$Z > 7 & zeb_las$Z <= 8) / sum(zeb_las$Z >= 0 & zeb_las$Z <= 8))
     ) %>%
     add_column(campaign, plot, .before = 1)
   
@@ -320,8 +367,8 @@ banner_summary <- banner_data %>%
   group_by(Plot) %>%
   summarize(
     trad_ladder_fuel_1to2 = mean(trad_ladder_fuel_1to2, na.rm = TRUE),
-    trad_ladder_fuel_1to3 = mean(trad_ladder_fuel_1to3, na.rm = TRUE),
-    trad_ladder_fuel_1to4 = mean(trad_ladder_fuel_1to4, na.rm = TRUE)
+    trad_ladder_fuel_2to3 = mean(trad_ladder_fuel_2to3, na.rm = TRUE),
+    trad_ladder_fuel_3to4 = mean(trad_ladder_fuel_3to4, na.rm = TRUE)
   ) %>%
   rename(plot=Plot) %>%
   mutate_at('plot', as.numeric)
@@ -331,14 +378,13 @@ banner_summary <- banner_data %>%
 combined_metrics <- tls_combine %>%
   full_join(als_combine, by = c('campaign','plot')) %>%
   full_join(zeb_combine, by = c('campaign','plot')) %>%
-  full_join(uav_combine, by = c('campaign','plot')) %>%
+  full_join(uas_combine, by = c('campaign','plot')) %>%
   full_join(banner_summary, by = 'plot')
 
-combined_metrics_filter <- tls_combine_filter
-%>%
+combined_metrics_filter <- tls_combine_filter %>%
   full_join(als_combine_filter, by = c('campaign','plot')) %>%
   full_join(zeb_combine_filter, by = c('campaign','plot')) %>%
-  full_join(uav_combine_filter, by = c('campaign','plot')) %>%
+  full_join(uas_combine_filter, by = c('campaign','plot')) %>%
   full_join(banner_summary, by = 'plot')
 
 write.csv(combined_metrics, out_file)
