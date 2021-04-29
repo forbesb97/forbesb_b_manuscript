d9e28eb3b516437128fc8d4a8e08d5097e80d8d8
Brieanne <forbesb@sonoma.com>
2021-04-28 18:43:09 -0700

    add all other metrics to histogram

--- ladder_fuel_distribution.R
+++ ladder_fuel_distribution.R
@@ -13,45 +13,70 @@
 library(tidyverse)
 library(ggplot2)
 library(glue)
+library(ggpubr)
 
 # ================================= User inputs ================================
-tls_file <- read_csv('D:/Analyses/ladder_fuel_full_process_take4/not_filtered/ladder_fuel_tls_210331.csv') 
+tls_file <- read_csv('D:/Analyses/ladder_fuel_jmp/ladder-fuels_metrics_tls_zeb_als_uas_filtered_burned_210426.csv') 
 
-histogram_out <- 'D:/Analyses/ladder_fuel_full_process_take4/not_filtered/ladder_fuel_distribution_histogram_210408'
-bar_out <- 'D:/Analyses/ladder_fuel_full_process_take4/not_filtered/ladder_fuel_distribution_barplot_210408'
+histogram_out <- 'D:/Analyses/ladder_fuel_jmp/ladder_fuel_distribution_histogram_filtered_210427.png'
+bar_out <- 'D:/Analyses/ladder_fuel_jmp/ladder_fuel_distribution_barplot_filtered_210427.png'
 
 # ============================ wrangle the data ================================
 tls_1to2 <- tls_file %>%
-  select(plot, RBR_3x3avg, ladder_fuel_1to2)%>%
-  rename(ladder_fuel=ladder_fuel_1to2)%>%
+  select(plot, rbr_class, tls_ladder_fuel_1to2)%>%
+  rename(ladder_fuel=tls_ladder_fuel_1to2)%>%
   add_column(variable='1to2')
 
-tls_1to3 <- tls_file %>%
-  select(plot, RBR_3x3avg, ladder_fuel_1to3)%>%
-  rename(ladder_fuel=ladder_fuel_1to3)%>%
-  add_column(variable='1to3')
+tls_2to3 <- tls_file %>%
+  select(plot, rbr_class, tls_ladder_fuel_2to3)%>%
+  rename(ladder_fuel=tls_ladder_fuel_2to3)%>%
+  add_column(variable='2to3')
+
+tls_3to4 <- tls_file %>%
+  select(plot, rbr_class, tls_ladder_fuel_3to4)%>%
+  rename(ladder_fuel=tls_ladder_fuel_3to4)%>%
+  add_column(variable='3to4')
+
+tls_4to5 <- tls_file %>%
+  select(plot, rbr_class, tls_ladder_fuel_4to5)%>%
+  rename(ladder_fuel=tls_ladder_fuel_4to5)%>%
+  add_column(variable='4to5')
+
+tls_5to6 <- tls_file %>%
+  select(plot, rbr_class, tls_ladder_fuel_5to6)%>%
+  rename(ladder_fuel=tls_ladder_fuel_5to6)%>%
+  add_column(variable='5to6')
+
+tls_6to7 <- tls_file %>%
+  select(plot, rbr_class, tls_ladder_fuel_6to7)%>%
+  rename(ladder_fuel=tls_ladder_fuel_6to7)%>%
+  add_column(variable='6to7')
 
 tls_7to8 <- tls_file %>%
-  select(plot, RBR_3x3avg, ladder_fuel_7to8)%>%
-  rename(ladder_fuel=ladder_fuel_7to8)%>%
+  select(plot, rbr_class, tls_ladder_fuel_7to8)%>%
+  rename(ladder_fuel=tls_ladder_fuel_7to8)%>%
   add_column(variable='7to8')
   
 tls_file2 <- tls_1to2 %>%
-  add_row(tls_1to3) %>%
+  add_row(tls_2to3) %>%
+  add_row(tls_3to4) %>%
+  add_row(tls_4to5) %>%
+  add_row(tls_5to6) %>%
+  add_row(tls_6to7)
   add_row(tls_7to8)
 
 #===============================Set theme=======================================
 theme_set(
   theme(
     text = element_text(family = 'serif', face = 'plain'),
-    axis.title = element_text(size = 16),
-    axis.text = element_text(size = 16),
+    axis.title = element_text(size = 24),
+    axis.text = element_text(size = 24),
     line = element_line(size = 1.5),
     axis.line = element_line(size = 1.5),
     panel.background = element_rect(color = 'white'),
     panel.border = element_rect(colour = 'black', fill = NA, size = 1.5),
-    legend.title = element_text(size = 24),
-    legend.text = element_text(size = 24),
+    legend.title = element_text(size = 28),
+    legend.text = element_text(size = 28),
     legend.spacing = unit(0, "cm"),
     legend.margin = margin(0, 5, 0, 5),
     legend.text.align = 0, 
@@ -59,23 +84,93 @@ theme_set(
   )
 )
 
-tls_file2$rbr_class <-
-  cut(
-    tls_file2$RBR_3x3avg,
-    breaks = c( 0,  35, 130, 298, Inf),
-    label = c('NC', 'Low', 'Medium', 'High')
-  )
-#tls_file$rbr_class <- factor(tls_file$rbr_class, levels = c("High", "Medium", "Low", "NC"))
 
 # ================================ histogram ===================================
 
+tls_file$rbr_class <- factor(tls_file$rbr_class, levels = c('NC', 'Low', 'Medium', 'High'))
 
-histogram <-ggplot(data=tls_file, aes(x=ladder_fuel_1to2, fill=rbr_class)) + 
-  geom_histogram(binwidth = 0.05)+
-  scale_color_manual(values = c('green3', 'yellow2','orange' , 'red'))+
-  scale_fill_manual(values = c('green3', 'yellow2','orange' , 'red'))
-histogram
+histogram_1to2 <-ggplot(data=tls_file, aes(x=tls_ladder_fuel_1to2, fill=rbr_class)) + 
+  geom_histogram(binwidth = 0.025)+
+  facet_wrap(~rbr_class)+
+  scale_fill_manual(values = c('green3', 'yellow2','orange' , 'red'), limits=c('NC', 'Low', 'Medium', 'High')) +
+  xlab('Ladder Fuel 1-2m')+
+  ylab('Count')+
+  theme(legend.position = 'none', strip.text.x = element_text(size = 16))
+histogram_1to2
 
+histogram_2to3 <-ggplot(data=tls_file, aes(x=tls_ladder_fuel_2to3, fill=rbr_class)) + 
+  geom_histogram(binwidth = 0.025)+
+  facet_wrap(~rbr_class)+
+  scale_fill_manual(values = c('green3', 'yellow2','orange' , 'red'), limits=c('NC', 'Low', 'Medium', 'High')) +
+  xlab('Ladder Fuel 2-3m')+
+  ylab('Count')+
+  theme(legend.position = 'none', strip.text.x = element_text(size = 16))
+histogram_2to3
+
+histogram_3to4 <-ggplot(data=tls_file, aes(x=tls_ladder_fuel_3to4, fill=rbr_class)) + 
+  geom_histogram(binwidth = 0.025)+
+  facet_wrap(~rbr_class)+
+  scale_fill_manual(values = c('green3', 'yellow2','orange' , 'red'), limits=c('NC', 'Low', 'Medium', 'High')) +
+  xlab('Ladder Fuel 3-4m')+
+  ylab('Count')+
+  theme(legend.position = 'none', strip.text.x = element_text(size = 16))
+histogram_3to4
+
+histogram_4to5 <-ggplot(data=tls_file, aes(x=tls_ladder_fuel_4to5, fill=rbr_class)) + 
+  geom_histogram(binwidth = 0.025)+
+  facet_wrap(~rbr_class)+
+  scale_fill_manual(values = c('green3', 'yellow2','orange' , 'red'), limits=c('NC', 'Low', 'Medium', 'High')) +
+  xlab('Ladder Fuel 4-5m')+
+  ylab('Count')+
+  theme(legend.position = 'none', strip.text.x = element_text(size = 16))
+histogram_4to5
+
+histogram_5to6 <-ggplot(data=tls_file, aes(x=tls_ladder_fuel_5to6, fill=rbr_class)) + 
+  geom_histogram(binwidth = 0.025)+
+  facet_wrap(~rbr_class)+
+  scale_fill_manual(values = c('green3', 'yellow2','orange' , 'red'), limits=c('NC', 'Low', 'Medium', 'High')) +
+  xlab('Ladder Fuel 5-6m')+
+  ylab('Count')+
+  theme(legend.position = 'none', strip.text.x = element_text(size = 16))
+histogram_5to6
+
+histogram_6to7 <-ggplot(data=tls_file, aes(x=tls_ladder_fuel_6to7, fill=rbr_class)) + 
+  geom_histogram(binwidth = 0.025)+
+  facet_wrap(~rbr_class)+
+  scale_fill_manual(values = c('green3', 'yellow2','orange' , 'red'), limits=c('NC', 'Low', 'Medium', 'High')) +
+  xlab('Ladder Fuel 6-7m')+
+  ylab('Count')+
+  theme(legend.position = 'none', strip.text.x = element_text(size = 16))
+histogram_6to7
+
+histogram_7to8<-ggplot(data=tls_file, aes(x=tls_ladder_fuel_7to8, fill=rbr_class)) + 
+  geom_histogram(binwidth = 0.025)+
+  facet_wrap(~rbr_class)+
+  scale_fill_manual(values = c('green3', 'yellow2','orange' , 'red'), limits=c('NC', 'Low', 'Medium', 'High')) +
+  xlab('Ladder Fuel 7-8m')+
+  ylab('Count')+
+  theme(legend.position = 'none', strip.text.x = element_text(size = 16))
+histogram_7to8
+
+histogram <- ggarrange(histogram_1to2, 
+                       histogram_2to3, 
+                       histogram_3to4, 
+                       histogram_4to5, 
+                       histogram_5to6, 
+                       histogram_6to7, 
+                       histogram_7to8,
+                       ncol = 4,
+                       nrow = 2,
+                       widths = c(6, 6, 6, 6),
+                       heights= c(6, 6))
+
+ggsave(
+  histogram_out,
+  plot=histogram,
+  width = 26,
+  height = 13,
+  units = 'in',
+  dpi = 300 )
 
 # ================================ bar plot ====================================
 
