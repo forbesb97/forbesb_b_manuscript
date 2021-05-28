c99bed77a92098737490d10d532fca84666ab84a
Brieanne <forbesb@sonoma.com>
2021-05-28 13:19:29 -0700

    added new variables to matrix

--- ladder_fuel_heat_map(4).R
+++ ladder_fuel_heat_map(4).R
@@ -24,7 +24,7 @@ library(glue)
 
 # ================================= User inputs ================================
 
-data <- read_csv('D:/Analyses/ladder_fuel_full_process_take4/not_filtered/ladder-fuels_metrics_tls_zeb_als_uas_banner_210326.csv')
+data <- read_csv('D:/Analyses/ladder_fuel_jmp/ladder-fuels_metrics_tls_zeb_als_uas_filtered_offset_210524.csv')
 #data <- read_csv('D:/Analyses/ladder_fuel_full_process_take4/filtered_zero_and_above/ladder-fuels_metrics_tls_zeb_als_uas_banner_filtered_210326.csv')
 
 
@@ -35,14 +35,14 @@ control_plots = c('1301','1303','1304','1306','1307','1309','1310','1312','1313'
 
 methods <- c('ALS', 'HMLS', 'UAS', 'Banner') #do not include tls, pulled out separately 
 
-outfile <- 'D:/Analyses/ladder_fuel_full_process_take4/not_filtered/ladder-fuel_heatmap_210331.png'
+outfile <- 'D:/Analyses/ladder_fuel_jmp/ladder-fuel_heatmap_210528.png'
 # outfile <- 'D:/Analyses/ladder_fuel_full_process_take4/filtered_zero_and_above/ladder-fuel_heatmap_filtered_210231.png'
 
-tidy_tls_outfile <- 'D:/Analyses/ladder_fuel_full_process_take4/not_filtered/ladder_fuel_tls_210331.csv'
-tidy_zeb_outfile <- 'D:/Analyses/ladder_fuel_full_process_take4/not_filtered/ladder_fuel_hmls_210331.csv'
-tidy_uas_outfile <- 'D:/Analyses/ladder_fuel_full_process_take4/not_filtered/ladder_fuel_uas_210331.csv'
-tidy_als_outfile <- 'D:/Analyses/ladder_fuel_full_process_take4/not_filtered/ladder_fuel_als_210331.csv'
-tidy_banner_outfile <- 'D:/Analyses/ladder_fuel_full_process_take4/not_filtered/ladder_fuel_banner_210331.csv'
+# tidy_tls_outfile <- 'D:/Analyses/ladder_fuel_full_process_take4/not_filtered/ladder_fuel_tls_210331.csv'
+# tidy_zeb_outfile <- 'D:/Analyses/ladder_fuel_full_process_take4/not_filtered/ladder_fuel_hmls_210331.csv'
+# tidy_uas_outfile <- 'D:/Analyses/ladder_fuel_full_process_take4/not_filtered/ladder_fuel_uas_210331.csv'
+# tidy_als_outfile <- 'D:/Analyses/ladder_fuel_full_process_take4/not_filtered/ladder_fuel_als_210331.csv'
+# tidy_banner_outfile <- 'D:/Analyses/ladder_fuel_full_process_take4/not_filtered/ladder_fuel_banner_210331.csv'
 
 # tidy_tls_outfile <- 'D:/Analyses/ladder_fuel_full_process_take4/filtered_zero_and_above/ladder_fuel_tls_filtered_210331.csv'
 # tidy_zeb_outfile <- 'D:/Analyses/ladder_fuel_full_process_take4/filtered_zero_and_above/ladder_fuel_hmls_filtered_210331.csv'
@@ -56,49 +56,68 @@ tidy_banner_outfile <- 'D:/Analyses/ladder_fuel_full_process_take4/not_filtered/
 
 
 tidy_data_tls <- data %>%
-  select(plot, tls_ladder_fuel_1to2,tls_ladder_fuel_1to3, 
-         tls_ladder_fuel_1to4,tls_ladder_fuel_7to8) %>% 
+  select(plot, tls_ladder_fuel_1to2,tls_ladder_fuel_2to3, 
+         tls_ladder_fuel_3to4,tls_ladder_fuel_4to5,
+         tls_ladder_fuel_5to6,tls_ladder_fuel_6to7,
+         tls_ladder_fuel_7to8) %>% 
   add_column(method = 'TLS', .after = 'plot') %>%
   rename(ladder_fuel_1to2=tls_ladder_fuel_1to2) %>%
-  rename(ladder_fuel_1to3=tls_ladder_fuel_1to3) %>%
-  rename(ladder_fuel_1to4=tls_ladder_fuel_1to4) %>%
+  rename(ladder_fuel_2to3=tls_ladder_fuel_2to3) %>%
+  rename(ladder_fuel_3to4=tls_ladder_fuel_3to4) %>%
+  rename(ladder_fuel_4to5=tls_ladder_fuel_4to5) %>%
+  rename(ladder_fuel_5to6=tls_ladder_fuel_5to6) %>%
+  rename(ladder_fuel_6to7=tls_ladder_fuel_6to7) %>%
   rename(ladder_fuel_7to8=tls_ladder_fuel_7to8)
 
 tidy_data_als <- data %>%
-  select(plot, als_ladder_fuel_1to2,als_ladder_fuel_1to3, 
-         als_ladder_fuel_1to4,als_ladder_fuel_7to8) %>% 
+  select(plot, als_ladder_fuel_1to2,als_ladder_fuel_2to3, 
+         als_ladder_fuel_3to4,als_ladder_fuel_4to5,
+         als_ladder_fuel_5to6,als_ladder_fuel_6to7,
+         als_ladder_fuel_7to8) %>% 
   add_column(method = 'ALS', .after = 'plot') %>%
   rename(ladder_fuel_1to2=als_ladder_fuel_1to2) %>%
-  rename(ladder_fuel_1to3=als_ladder_fuel_1to3) %>%
-  rename(ladder_fuel_1to4=als_ladder_fuel_1to4) %>%
+  rename(ladder_fuel_2to3=als_ladder_fuel_2to3) %>%
+  rename(ladder_fuel_3to4=als_ladder_fuel_3to4) %>%
+  rename(ladder_fuel_4to5=als_ladder_fuel_4to5) %>%
+  rename(ladder_fuel_5to6=als_ladder_fuel_5to6) %>%
+  rename(ladder_fuel_6to7=als_ladder_fuel_6to7) %>%
   rename(ladder_fuel_7to8=als_ladder_fuel_7to8)
 
 tidy_data_zeb <- data %>%
-  select(plot, zeb_ladder_fuel_1to2,zeb_ladder_fuel_1to3, 
-         zeb_ladder_fuel_1to4,zeb_ladder_fuel_7to8) %>% 
+  select(plot, zeb_ladder_fuel_1to2,zeb_ladder_fuel_2to3, 
+         zeb_ladder_fuel_3to4,zeb_ladder_fuel_4to5,
+         zeb_ladder_fuel_5to6,zeb_ladder_fuel_6to7,
+         zeb_ladder_fuel_7to8) %>% 
   add_column(method = 'HMLS', .after = 'plot') %>%
   rename(ladder_fuel_1to2=zeb_ladder_fuel_1to2) %>%
-  rename(ladder_fuel_1to3=zeb_ladder_fuel_1to3) %>%
-  rename(ladder_fuel_1to4=zeb_ladder_fuel_1to4) %>%
+  rename(ladder_fuel_2to3=zeb_ladder_fuel_2to3) %>%
+  rename(ladder_fuel_3to4=zeb_ladder_fuel_3to4) %>%
+  rename(ladder_fuel_4to5=zeb_ladder_fuel_4to5) %>%
+  rename(ladder_fuel_5to6=zeb_ladder_fuel_5to6) %>%
+  rename(ladder_fuel_6to7=zeb_ladder_fuel_6to7) %>%
   rename(ladder_fuel_7to8=zeb_ladder_fuel_7to8)
 
 tidy_data_uas <- data %>%
-  select(plot, uav_ladder_fuel_1to2,uav_ladder_fuel_1to3, 
-         uav_ladder_fuel_1to4,uav_ladder_fuel_7to8) %>% 
+  select(plot, uas_ladder_fuel_1to2,uas_ladder_fuel_2to3, 
+         uas_ladder_fuel_3to4,uas_ladder_fuel_4to5,
+         uas_ladder_fuel_5to6,uas_ladder_fuel_6to7,
+         uas_ladder_fuel_7to8) %>% 
   add_column(method = 'UAS', .after = 'plot') %>%
-  rename(ladder_fuel_1to2=uav_ladder_fuel_1to2) %>%
-  rename(ladder_fuel_1to3=uav_ladder_fuel_1to3) %>%
-  rename(ladder_fuel_1to4=uav_ladder_fuel_1to4) %>%
-  rename(ladder_fuel_7to8=uav_ladder_fuel_7to8)
+  rename(ladder_fuel_1to2=uas_ladder_fuel_1to2) %>%
+  rename(ladder_fuel_2to3=uas_ladder_fuel_2to3) %>%
+  rename(ladder_fuel_3to4=uas_ladder_fuel_3to4) %>%
+  rename(ladder_fuel_4to5=uas_ladder_fuel_4to5) %>%
+  rename(ladder_fuel_5to6=uas_ladder_fuel_5to6) %>%
+  rename(ladder_fuel_6to7=uas_ladder_fuel_6to7) %>%
+  rename(ladder_fuel_7to8=uas_ladder_fuel_7to8)
 
 tidy_data_banner <- data %>%
-  select(plot, trad_ladder_fuel_1to2,
-         trad_ladder_fuel_1to3,
-         trad_ladder_fuel_1to4) %>%
+  select(plot, trad_ladder_fuel_1to2,trad_ladder_fuel_2to3, 
+         trad_ladder_fuel_3to4) %>%
   add_column(method = 'Banner', .after = 'plot') %>%
   rename(ladder_fuel_1to2=trad_ladder_fuel_1to2) %>%
-  rename(ladder_fuel_1to3=trad_ladder_fuel_1to3) %>%
-  rename(ladder_fuel_1to4=trad_ladder_fuel_1to4) 
+  rename(ladder_fuel_2to3=trad_ladder_fuel_2to3) %>%
+  rename(ladder_fuel_3to4=trad_ladder_fuel_3to4) 
 
 tidy_data <- tidy_data_tls %>%
   add_row(tidy_data_als)%>%
@@ -123,10 +142,16 @@ for (i in methods) {
   if (i == 'Banner') {  
   metric_1to2 <-
     cor(tls$ladder_fuel_1to2,  method_data$ladder_fuel_1to2, use='na.or.complete')
-  metric_1to3 <-
-    cor(tls$ladder_fuel_1to3, method_data$ladder_fuel_1to3, use='na.or.complete')
-  metric_1to4 <-
-    cor(tls$ladder_fuel_1to4,  method_data$ladder_fuel_1to4, use='na.or.complete')
+  metric_2to3 <-
+    cor(tls$ladder_fuel_2to3, method_data$ladder_fuel_2to3, use='na.or.complete')
+  metric_3to4 <-
+    cor(tls$ladder_fuel_3to4,  method_data$ladder_fuel_3to4, use='na.or.complete')
+  metric_4to5 <-
+    NA
+  metric_5to6 <-
+    NA
+  metric_6to7 <-
+    NA
   metric_7to8 <-
     NA
   
@@ -135,31 +160,40 @@ for (i in methods) {
   lm_model_coef_1to2 = summary(lm_model_1to2)$coefficients
   p.value_1to2 <-  lm_model_coef_1to2[2, 'Pr(>|t|)']
   
-  lm_model_1to3 <- lm(
-    formula = tls$ladder_fuel_1to3 ~ method_data$ladder_fuel_1to3)
-  lm_model_coef_1to3 = summary(lm_model_1to3)$coefficients
-  p.value_1to3 <-  lm_model_coef_1to3[2, 'Pr(>|t|)']
+  lm_model_2to3 <- lm(
+    formula = tls$ladder_fuel_2to3 ~ method_data$ladder_fuel_2to3)
+  lm_model_coef_2to3 = summary(lm_model_2to3)$coefficients
+  p.value_2to3 <-  lm_model_coef_2to3[2, 'Pr(>|t|)']
   
-  lm_model_1to4 <- lm(
-    formula = tls$ladder_fuel_1to4 ~ method_data$ladder_fuel_1to4)
-  lm_model_coef_1to4 = summary(lm_model_1to4)$coefficients
-  p.value_1to4 <-  lm_model_coef_1to4[2, 'Pr(>|t|)']
+  lm_model_3to4 <- lm(
+    formula = tls$ladder_fuel_3to4 ~ method_data$ladder_fuel_3to4)
+  lm_model_coef_3to4 = summary(lm_model_3to4)$coefficients
+  p.value_3to4 <-  lm_model_coef_3to4[2, 'Pr(>|t|)']
   
+  p.value_4to5 <-  NA
+  p.value_5to6 <-  NA
+  p.value_6to7 <-  NA
   p.value_7to8 <-  NA
   
   correlation <- tibble(
     var1 = i,
-    var2 = c('1-2m', '1-3m', '1-4m', '7-8m'),
+    var2 = c('1-2m', '2-3m', '3-4m', '4-5m', '5-6m', '6-7m', '7-8m'),
     correlation = c(
       metric_1to2,
-      metric_1to3,
-      metric_1to4,
+      metric_2to3,
+      metric_3to4,
+      metric_4to5,
+      metric_5to6,
+      metric_6to7,
       metric_7to8
     ),
     p.value = c(
       p.value_1to2,
-      p.value_1to3,
-      p.value_1to4,
+      p.value_2to3,
+      p.value_3to4,
+      p.value_4to5,
+      p.value_5to6,
+      p.value_6to7,
       p.value_7to8
     )
   )
@@ -169,10 +203,16 @@ for (i in methods) {
     
   metric_1to2 <-
     cor(tls$ladder_fuel_1to2, method_data$ladder_fuel_1to2, use='na.or.complete')
-  metric_1to3 <-
-    cor(tls$ladder_fuel_1to3, method_data$ladder_fuel_1to3, use='na.or.complete')
-  metric_1to4 <-
-    cor(tls$ladder_fuel_1to4, method_data$ladder_fuel_1to4, use='na.or.complete')
+  metric_2to3 <-
+    cor(tls$ladder_fuel_2to3, method_data$ladder_fuel_2to3, use='na.or.complete')
+  metric_3to4 <-
+    cor(tls$ladder_fuel_3to4, method_data$ladder_fuel_3to4, use='na.or.complete')
+  metric_4to5 <-
+    cor(tls$ladder_fuel_4to5, method_data$ladder_fuel_4to5, use='na.or.complete')
+  metric_5to6 <-
+    cor(tls$ladder_fuel_5to6, method_data$ladder_fuel_5to6, use='na.or.complete')
+  metric_3to4 <-
+    cor(tls$ladder_fuel_6to7, method_data$ladder_fuel_6to7, use='na.or.complete')
   metric_7to8 <-
     cor(tls$ladder_fuel_7to8, method_data$ladder_fuel_7to8, use='na.or.complete')
   
@@ -181,15 +221,30 @@ for (i in methods) {
   lm_model_coef_1to2 = summary(lm_model_1to2)$coefficients
   p.value_1to2 <-  lm_model_coef_1to2[2, 'Pr(>|t|)']
   
-  lm_model_1to3 <- lm(
-    formula = tls$ladder_fuel_1to3 ~ method_data$ladder_fuel_1to3)
-  lm_model_coef_1to3 = summary(lm_model_1to3)$coefficients
-  p.value_1to3 <-  lm_model_coef_1to3[2, 'Pr(>|t|)']
+  lm_model_2to3 <- lm(
+    formula = tls$ladder_fuel_2to3 ~ method_data$ladder_fuel_2to3)
+  lm_model_coef_2to3 = summary(lm_model_2to3)$coefficients
+  p.value_2to3 <-  lm_model_coef_2to3[2, 'Pr(>|t|)']
   
-  lm_model_1to4 <- lm(
-    formula = tls$ladder_fuel_1to4 ~ method_data$ladder_fuel_1to4)
-  lm_model_coef_1to4 = summary(lm_model_1to4)$coefficients
-  p.value_1to4 <-  lm_model_coef_1to4[2, 'Pr(>|t|)']
+  lm_model_3to4 <- lm(
+    formula = tls$ladder_fuel_3to4 ~ method_data$ladder_fuel_3to4)
+  lm_model_coef_3to4 = summary(lm_model_3to4)$coefficients
+  p.value_3to4 <-  lm_model_coef_3to4[2, 'Pr(>|t|)']
+  
+  lm_model_4to5 <- lm(
+    formula = tls$ladder_fuel_4to5 ~ method_data$ladder_fuel_4to5)
+  lm_model_coef_4to5 = summary(lm_model_4to5)$coefficients
+  p.value_4to5 <-  lm_model_coef_4to5[2, 'Pr(>|t|)']
+  
+  lm_model_5to6 <- lm(
+    formula = tls$ladder_fuel_5to6 ~ method_data$ladder_fuel_5to6)
+  lm_model_coef_5to6 = summary(lm_model_5to6)$coefficients
+  p.value_5to6 <-  lm_model_coef_5to6[2, 'Pr(>|t|)']
+  
+  lm_model_6to7 <- lm(
+    formula = tls$ladder_fuel_6to7 ~ method_data$ladder_fuel_6to7)
+  lm_model_coef_6to7 = summary(lm_model_6to7)$coefficients
+  p.value_6to7 <-  lm_model_coef_6to7[2, 'Pr(>|t|)']
 
   lm_model_7to8 <- lm(
     formula = tls$ladder_fuel_7to8 ~ method_data$ladder_fuel_7to8)
@@ -198,17 +253,23 @@ for (i in methods) {
 
   correlation <- tibble(
     var1 = i,
-    var2 = c('1-2m', '1-3m', '1-4m', '7-8m'),
+    var2 = c('1-2m', '2-3m', '3-4m', '4-5m', '5-6m', '6-7m', '7-8m'),
     correlation = c(
       metric_1to2,
-      metric_1to3,
-      metric_1to4,
+      metric_2to3,
+      metric_3to4,
+      metric_4to5,
+      metric_5to6,
+      metric_6to7,
       metric_7to8
     ),
     p.value = c(
       p.value_1to2,
-      p.value_1to3,
-      p.value_1to4,
+      p.value_2to3,
+      p.value_3to4,
+      p.value_4to5,
+      p.value_5to6,
+      p.value_6to7,
       p.value_7to8
     )
   )
